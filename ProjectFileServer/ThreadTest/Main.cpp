51efd7c0c4b29859ec953b6cbbb2af2bdca95a07
Thanh Nguyen <thanhqng1510@gmail.com>
2020-06-26 12:38:57 +0700

    .

--- ProjectFileServer/ThreadTest/Main.cpp
+++ ProjectFileServer/ThreadTest/Main.cpp
@@ -1,80 +1,51 @@
-//#include <atomic>
-//#include <thread>
-//#include <iostream>
-//#include <chrono>
-//
-//
-//void ReadCin(std::atomic<int>& disp_mode) {
-//    int inp;
-//
-//    while (disp_mode.load() != 3) {
-//        std::cin >> inp;
-//        disp_mode.store(inp);
-//
-//        system("cls");
-//    }
-//}
-//
-//
-//int main() {
-//    std::atomic<int> disp_mode(1);
-//    // 1 -> activity
-//    // 2 -> list
-//    // 3 -> quit
-//
-//    std::thread cin_thread(ReadCin, std::ref(disp_mode));
-//
-//    while (disp_mode.load() != 3) {
-//        if (disp_mode.load() == 1)
-//            std::cout << "activity mode\n";
-//        else    // 2
-//            std::cout << "list mode\n";
-//
-//        std::this_thread::sleep_for(std::chrono::seconds(1));
-//    }
-//
-//    cin_thread.join();
-//}
-
-
-#include <iostream>
+#include <atomic>
 #include <thread>
-#include <string>
+#include <iostream>
+#include <chrono>
 #include <mutex>
 #include <conio.h>
-#include <chrono>
 
 
 std::mutex mtx;
 
-void CinThread() {
-    while (true) {
+void CinThread(std::atomic<int>& disp_mode) {
+    while (disp_mode.load() != 3) {
         char c = _getch();
 
         mtx.lock();
 
-        while ((int)c != 13)
-            std::cout << c << "\n";
+        std::cout << c;
+        disp_mode.store((int)c - 48);
+        c = _getch();
+        system("cls");
 
         mtx.unlock();
-    }
-}
-
-void CoutThread() {
-    while (true) {
-        mtx.lock();
-        std::cout << "cout_thread\n";
-        mtx.unlock();
-
-        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 }
 
 
 int main() {
-    std::thread cin_thread(CinThread);
-    std::thread cout_thread(CoutThread);
+    std::atomic<int> disp_mode(1);
+    // 1 -> activity
+    // 2 -> list
+    // 3 -> quit
+
+    std::thread cin_thread(CinThread, std::ref(disp_mode));
+
+    while (disp_mode.load() != 3) {
+        if (disp_mode.load() == 1) {
+            mtx.lock();
+            std::cout << "activity mode\n";
+            mtx.unlock();
+        }
+        else {    // 2
+            mtx.lock();
+            std::cout << "list mode\n";
+            mtx.unlock();
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
 
     cin_thread.join();
-    cout_thread.join();
 }
